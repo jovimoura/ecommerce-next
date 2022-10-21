3677e5ecd671a0ab69b3192e0c01c579fe4859d1
jovimoura <joaovictors.mouraa@gmail.com>
2022-10-21 09:48:38 -0300

    navbar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { AuthProvider } from '../contexts/AuthContext'
+import { Navbar } from '../components/Navbar'
+import { useRouter } from 'next/router'
 
 interface CustomPageProps {
   session: any
@@ -11,9 +13,12 @@ function MyApp({
   Component,
   pageProps: { session, ...pageProps }
 }: AppProps<CustomPageProps>) {
+  const router = useRouter()
+
   return (
     <AuthProvider>
       <SessionProvider session={session}>
+        {router.asPath !== '/' ? <Navbar /> : null}
         <Component {...pageProps} />
       </SessionProvider>
     </AuthProvider>
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,22 +1,10 @@
 import { Fragment, useContext, useEffect, useState } from 'react'
 import Head from 'next/head'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/20/solid'
 import { GetServerSideProps } from 'next'
 import { parseCookies } from 'nookies'
-import { signOut } from 'next-auth/react'
 import { AuthContext } from '../../contexts/AuthContext'
 import { api } from '../../services/api'
-import { getAPIClient } from '../../services/axios'
 import { prisma } from '../../lib/prisma'
-
-const navigation = ['Dashboard', 'Team', 'Projects', 'Calendar', 'Reports']
-
-const profile = ['Your Profile', 'Settings']
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ')
-}
 
 interface Task {
   id: string
@@ -46,199 +34,8 @@ export default function Dashboard({ tasks }: Props) {
     <div>
       <Head>
         <title>Dashboard</title>
+        <link rel="shortcut icon" href="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" type="image/x-icon" />
       </Head>
-
-      <Disclosure as="nav" className="bg-gray-800">
-        {({ open }) => (
-          <>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="flex items-center justify-between h-16">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-8 w-8"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                      alt="Workflow"
-                    />
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline space-x-4">
-                      {navigation.map((item, itemIdx) =>
-                        itemIdx === 0 ? (
-                          <Fragment key={item}>
-                            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                            <a
-                              href="#"
-                              className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                            >
-                              {item}
-                            </a>
-                          </Fragment>
-                        ) : (
-                          <a
-                            key={item}
-                            href="#"
-                            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                          >
-                            {item}
-                          </a>
-                        )
-                      )}
-                    </div>
-                  </div>
-                </div>
-                <div className="hidden md:block">
-                  <div className="ml-4 flex items-center md:ml-6">
-                    <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-
-                    {/* Profile dropdown */}
-                    <Menu as="div" className="ml-3 relative">
-                      {({ open }) => (
-                        <>
-                          <div>
-                            <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                              <span className="sr-only">Open user menu</span>
-                              <img
-                                className="h-8 w-8 rounded-full"
-                                src={user?.avatar_url}
-                                alt=""
-                              />
-                            </Menu.Button>
-                          </div>
-                          <Transition
-                            show={open}
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                          >
-                            <Menu.Items
-                              static
-                              className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                            >
-                              {profile.map(item => (
-                                <Menu.Item key={item}>
-                                  {({ active }) => (
-                                    <a
-                                      href="#"
-                                      className={classNames(
-                                        active ? 'bg-gray-100' : '',
-                                        'block px-4 py-2 text-sm text-gray-700'
-                                      )}
-                                    >
-                                      {item}
-                                    </a>
-                                  )}
-                                </Menu.Item>
-                              ))}
-                              <Menu.Item>
-                                <button
-                                  onClick={() =>
-                                    signOut({
-                                      redirect: true,
-                                      callbackUrl: '/'
-                                    })
-                                  }
-                                  className="block px-4 py-2 text-sm text-gray-700"
-                                >
-                                  Sign out
-                                </button>
-                              </Menu.Item>
-                            </Menu.Items>
-                          </Transition>
-                        </>
-                      )}
-                    </Menu>
-                  </div>
-                </div>
-                <div className="-mr-2 flex md:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-              </div>
-            </div>
-
-            <Disclosure.Panel className="md:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                {navigation.map((item, itemIdx) =>
-                  itemIdx === 0 ? (
-                    <Fragment key={item}>
-                      {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                      <a
-                        href="#"
-                        className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-                      >
-                        {item}
-                      </a>
-                    </Fragment>
-                  ) : (
-                    <a
-                      key={item}
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      {item}
-                    </a>
-                  )
-                )}
-              </div>
-              <div className="pt-4 pb-3 border-t border-gray-700">
-                <div className="flex items-center px-5">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src={user?.avatar_url}
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <div className="text-base font-medium leading-none text-white">
-                      João Moura
-                    </div>
-                    <div className="text-sm font-medium leading-none text-gray-400">
-                      joãovictors.mouraa@gmail.com
-                    </div>
-                  </div>
-                  <button className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-                <div className="mt-3 px-2 space-y-1">
-                  {profile.map(item => (
-                    <a
-                      key={item}
-                      href="#"
-                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-                    >
-                      {item}
-                    </a>
-                  ))}
-                  <a
-                    href="#"
-                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-                  >
-                    Sign out
-                  </a>
-                </div>
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
 
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -247,7 +44,6 @@ export default function Dashboard({ tasks }: Props) {
       </header>
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {/* Replace with your content */}
           <div className="px-4 py-6 sm:px-0">
             <input
               type="text"
@@ -257,7 +53,6 @@ export default function Dashboard({ tasks }: Props) {
             />
             <button onClick={handleCreateTask}>Create</button>
           </div>
-          {/* /End replace */}
         </div>
       </main>
     </div>
@@ -265,7 +60,6 @@ export default function Dashboard({ tasks }: Props) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx: any) => {
-  // const apiClient = getAPIClient(ctx)
   const { 'auth_next-token': token } = parseCookies(ctx)
 
   if (!token) {
@@ -287,8 +81,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx: any) => {
       updatedAt: task.updatedAt.toISOString()
     }
   })
-  // uncomment when backend is working
-  // await apiClient.get('/users')
+
   return {
     props: {
       tasks: data
