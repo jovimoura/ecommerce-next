f5a17e10b99a3aacbda3f03466acf64d7a3eefcb
jovimoura <joaovictors.mouraa@gmail.com>
2022-11-02 18:01:08 -0300

    urls

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,8 +4,13 @@ import { parseCookies } from 'nookies'
 export function getAPIClient(ctx?: any) {
   const { 'auth_next-token': token } = parseCookies(ctx)
 
+  console.log(process.env.NODE_ENV as string)
+
   const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000'
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? (process.env.PROD_URL_API as string)
+        : (process.env.DEV_URL_API as string)
   })
 
   api.interceptors.request.use(config => {
