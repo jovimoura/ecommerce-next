3a9c93b1ac40505f99fa29ce554f59b0fe77c23b
jovimoura <joaovictors.mouraa@gmail.com>
2023-03-24 16:15:45 -0300

    fix padding home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -120,7 +120,7 @@ export default function Marketplace({ items }: Props) {
             Nossos <span className='text-indigo-500'>Destaques</span>
           </h1>
         </div>
-        <div className='h-full px-10 md:h-[calc(100vh-200px)] overflow-hidden'>
+        <div className='h-full px-10 py-4 md:h-[calc(100vh-200px)] overflow-hidden'>
           <div className='mt-6 px-0 sm:px-6 lg:px-8 flex flex-wrap justify-around items-center gap-10 md:gap-0'>
             {items?.map((item, i) => (
               <Item
